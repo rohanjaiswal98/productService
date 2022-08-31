cbfcf66ad801f154d0029cdfd22bad2ecc78dacf
Rohan Jaiswal <rohan.jaiswal71@gmail.com>
2022-08-31 15:04:56 +0530

    add auth

--- src/main/java/productRepo/ProductController.java
+++ src/main/java/productRepo/ProductController.java
@@ -1,16 +1,10 @@
 package productRepo;
 
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 class ProductController {
@@ -23,36 +17,24 @@ class ProductController {
 
 
     @GetMapping("/products")
-    CollectionModel<EntityModel<Product>> all() {
-
-        List<EntityModel<Product>> users = repository.findAll().stream()
-                .map(product -> EntityModel.of(product,
-                        linkTo(methodOn(ProductController.class).one(product.getId())).withSelfRel(),
-                        linkTo(methodOn(ProductController.class).all()).withRel("products")))
-                .collect(Collectors.toList());
-
-        return CollectionModel.of(users, linkTo(methodOn(ProductController.class).all()).withSelfRel());
+    List<Product> all() {
+        return repository.findAll();
     }
 
     @PostMapping("/products")
-    Product newUser(@RequestBody Product product) {
+    Product newProduct(@RequestBody Product product) {
         return repository.save(product);
     }
 
 
     @GetMapping("/products/{id}")
-    EntityModel<Product> one(@PathVariable Long id) {
+    Product one(@PathVariable Long id) {
 
-        Product product = repository.findById(id) //
-                .orElseThrow(() -> new ProductNotFoundException(id));
-
-        return EntityModel.of(product, //
-                linkTo(methodOn(ProductController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(ProductController.class).all()).withRel("products"));
+        return repository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
     }
 
     @PutMapping("/products/{id}")
-    Product updateUser(@RequestBody Product newProduct, @PathVariable Long id) {
+    Product updateProduct(@RequestBody Product newProduct, @PathVariable Long id) {
 
         return repository.findById(id) //
                 .map(product -> {
@@ -68,7 +50,7 @@ class ProductController {
     }
 
     @DeleteMapping("/products/{id}")
-    ResponseEntity<Product> deleteUser(@PathVariable Long id) {
+    ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
         Product product = repository.findById(id).map(p -> {
             repository.deleteById(id);
             return p;
